816dc137c7b56244fb24819eb185782d8539ee07
thisismrali <a.yousefi@airinmedia.com>
2019-06-22 09:06:53 +0430

    fixed scroll

--- src/app/animation/components/animation-item-loader/animation-item-loader.component.ts
+++ src/app/animation/components/animation-item-loader/animation-item-loader.component.ts
@@ -1,9 +1,10 @@
+import { Router } from '@angular/router';
 import { AnimationBuilderCategory } from './../../../shared/helpers/animationBuilder-category';
 import { AnimationBuilderItem, AnimationBuilderItemType } from './../../../shared/helpers/animationBuilder-item';
-import { Component, OnInit, Input, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy, ChangeDetectorRef, AfterViewInit } from '@angular/core';
 import { AnimationCoreService } from './../../services/animation-core.service';
 import { Subscription } from 'rxjs';
-
+import { ScrollStoreProvider } from "../../../shared/helper/reuse-strategy/scroll-reuse";
 
 @Component({
   selector: 'mf-animation-item-loader',
@@ -11,9 +12,10 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./animation-item-loader.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AnimationItemLoaderComponent implements OnInit {
+export class AnimationItemLoaderComponent implements OnInit,AfterViewInit {
   @Input() filter = 0;
   showDropDown = true;
+  private _scrollStoreProvider: ScrollStoreProvider;
   private _items = [];
   private itemsSubscibe: Subscription;
   oldFilter = -1;
@@ -21,7 +23,7 @@ export class AnimationItemLoaderComponent implements OnInit {
     return this._items.filter(item => this.itemHasType(item, this.filter));
   }
   indexTrackFn = (index: number, item: AnimationBuilderItem) => item.id;
-  constructor(private _animationCoreService: AnimationCoreService, private _changeDetectorRef: ChangeDetectorRef) {
+  constructor(private _animationCoreService: AnimationCoreService, private _changeDetectorRef: ChangeDetectorRef, private _router: Router) {
     this.itemsSubscibe = this._animationCoreService.showCategory.subscribe((category: AnimationBuilderCategory) => {
       if(!category) return;
       this._items = this._animationCoreService.getCategoryItems(category.categoryPath, category.type);
@@ -34,9 +36,29 @@ export class AnimationItemLoaderComponent implements OnInit {
       }
       _changeDetectorRef.markForCheck();
     });
+    this._scrollStoreProvider = new ScrollStoreProvider({
+      compContext: this,
+      router: this._router,
+      route: "/dashboard/animation"
+    });
   }
   itemHasType(item: AnimationBuilderItem, type: number): boolean {
     return item.types.some(t => t.type === this.filter);
   }
   ngOnInit(): void { }
+  ngAfterViewInit(): void {
+    // if (this._userManagerService.isLoggedin) {
+    // this._scrollStoreProvider.handleScroll(
+    //   "MainProject",
+    //   this.itemsHolder.nativeElement
+    // );
+    // }
+    // this._userManagerService.loginSubject.subscribe(isLoggedIn => {
+    //   if (isLoggedIn && !this._userManagerService.isLoggedin) {
+    setTimeout(() => {
+      this._scrollStoreProvider.handleScroll("MainProject", ".animation-item-loader-wrap");
+    });
+    // }
+    // });
+  }
 }
